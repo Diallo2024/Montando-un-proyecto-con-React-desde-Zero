17c006cc1234f42da2a62ebb9570e667bbd83da7
Diallo2024 <urymamadu@gmail.com>
2024-09-14 13:54:56 -0700

    added navbar with routes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import React from 'react'
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <>
+    <Navbar/>
     <Outlet/>
     </>
   )
